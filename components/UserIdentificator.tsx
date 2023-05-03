f686c12ad446e01e82a2121358081c6c48fa3c47
Anderson Lemke <amatheuslemke@hotmail.com>
2023-05-03 18:44:35 -0300

    feat: add user identificator

---
+++ components/UserIdentificator.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+
+const UserIdentificator: NextPage = () => {
+  return (
+    <div>
+      <label htmlFor="user">{'Quem é você'}</label>
+      <input />
+    </div>
+  )
+}
+
+export default UserIdentificator
