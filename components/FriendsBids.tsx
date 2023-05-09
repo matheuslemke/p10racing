38c298ecb5083423816e48e0859b81ddcb68ea16
Anderson Lemke <amatheuslemke@hotmail.com>
2023-05-09 10:05:14 -0300

    style: add rounded table

--- components/FriendsBids.tsx
+++ components/FriendsBids.tsx
@@ -11,49 +11,53 @@ const FriendsBids: NextPage<Props> = ({ users, bids }) => {
   return (
     <>
       <hr />
-      <h2 className='p-4'>Apostas dos amigos</h2>
-      <table>
-        <thead>
-          <tr>
-            {users.map((user) => (
-              <th key={user.id} colSpan={2}>
-                {user.name}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {users.map(() => (
-              <>
-                <th>P-10</th>
-                <th>1st Retirement</th>
-              </>
-            ))}
-          </tr>
-          <tr>
-            {users.map((user) => {
-              const userBid = bids.filter((bid) => bid.user === user.id).shift()
+      <h2 className="p-4">Apostas dos amigos</h2>
+      <div className="relative overflow-x-auto shadow-md rounded-lg">
+        <table>
+          <thead>
+            <tr>
+              {users.map((user) => (
+                <th key={user.id} colSpan={2}>
+                  {user.name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {users.map(() => (
+                <>
+                  <th>P-10</th>
+                  <th>1st Retirement</th>
+                </>
+              ))}
+            </tr>
+            <tr>
+              {users.map((user) => {
+                const userBid = bids
+                  .filter((bid) => bid.user === user.id)
+                  .shift()
 
-              if (!userBid) {
+                if (!userBid) {
+                  return (
+                    <>
+                      <td>-</td>
+                      <td>-</td>
+                    </>
+                  )
+                }
+
                 return (
                   <>
-                    <td>-</td>
-                    <td>-</td>
+                    <td>{userBid.p10.name}</td>
+                    <td>{userBid.first_retirement.name}</td>
                   </>
                 )
-              }
-
-              return (
-                <>
-                  <td>{userBid.p10.name}</td>
-                  <td>{userBid.first_retirement.name}</td>
-                </>
-              )
-            })}
-          </tr>
-        </tbody>
-      </table>
+              })}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
