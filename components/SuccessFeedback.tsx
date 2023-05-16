c165ea7f931064a9478406b6cf3b0e9e4df68af6
Anderson Lemke <amatheuslemke@hotmail.com>
2023-05-16 00:13:47 -0300

    feat: add success feedback

---
+++ components/SuccessFeedback.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const SuccessFeedback: NextPage = () => {
+  return (
+    <section className="flex items-center justify-center py-[210px]">
+      <label className="text-3xl leading-[38px]">Deu boa!</label>
+    </section>
+  )
+}
+
+export default SuccessFeedback
