0fb21fd397fbc25d884e6c53642c0d9cd492d85b
Anderson Lemke <amatheuslemke@hotmail.com>
2023-06-04 14:51:56 -0300

    feat: add ranking link on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 const Header: NextPage = () => {
   return (
-    <header className="border-b-2 border-gray-600 border-solid">
+    <header className="border-b-2 border-gray-600 border-solid flex">
       <h1 className="p-5 text-2xl">P10 da rapaziada</h1>
+      <span className='place-self-center ml-auto mr-5 text-sky-400 hover:underline'>
+        <Link href="/ranking">Ranking</Link>
+      </span>
     </header>
   )
 }
